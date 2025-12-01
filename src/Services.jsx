b3f904c038551e20cc47c3e6698333a1921d53ed
Mucassah <mucassah@Mucassahs-MacBook-Pro.local>
2025-12-01 16:04:03 +0300

    homepage

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,70 +1,70 @@
 import './Services.css';
-
+import { Link } from "react-router-dom";
 
 function Services(){
 
     return(
             <div className='Services'>
-                <a className='Estate' href='#'>
+                <Link className='Estate' to='/Esto'>
                     <div className='Estate'>
                         <h2>REAL ESTATE</h2>
                         
                     </div>
-                </a>
-                <a className='Farming' href='#'>
+                </Link>
+                <Link className='Farming' to='/Agribusiness'>
                     <div className='Farming'>
                         <h2>FARMING AND AGRIBUSINESS</h2>
                         
                     </div>
-                </a>
-                <a className='Security' href='#'>
+                </Link>
+                <Link className='Security' to='/Security'>
                     <div className='Security'>
                         <h2>SECURITY</h2>
                         
                     </div>
-                </a>
-                <a className='Insurance' href='#'>
+                </Link>
+                <Link className='Insurance' to='/Insurance'>
                     <div className='Insurance'>
                         <h2>INSURANCE</h2>
                         
                     </div>
-                </a>
-                <a className='Trade' href='#'>
+                </Link>
+                <Link className='Trade' to='Logistics'>
                     <div className='Trade'>
                         <h2>LOGISTICS</h2>
                         
                     </div>
-                </a>
-                <a className='Consulting' href='#'>
+                </Link>
+                <Link className='Consulting' to='/Consulting'>
                     <div className='Consulting'>
                         <h2>CONSULTING</h2>
                         
                     </div>
-                </a>
-                <a className='Golf' href='#'>
+                </Link>
+                <Link className='Golf' to='/Golf'>
                     <div className='Golf'>
                         <h2>GOLF</h2>
                         
                     </div>
-                </a>
-                <a className='Tv' href='#'>
+                </Link>
+                <Link className='Tv' to='Entertainment'>
                     <div className='Tv'>
                         <h2>ENTERTAINMENT</h2>
                         
                     </div>
-                </a>
-                <a className='Football' href='#'>
+                </Link>
+                <Link className='Football' to='/Football'>
                     <div className='Football'>
                         <h2>FOOTBALL CLUB</h2>
                         
                     </div>
-                </a>
-                <a className='Music' href='#'>
+                </Link>
+                <Link className='Music' to='/Music'>
                     <div className='Music'>
                         <h2>MUSIC</h2>
                         
                     </div>
-                </a>
+                </Link>
 
                 
             </div>
